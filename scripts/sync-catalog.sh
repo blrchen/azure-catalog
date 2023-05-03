db0418b7fe6283afadc571015429ab2d7d5663a4
Blair Chen <blrchen@hotmail.com>
2023-05-04 01:06:47 +0800

    Sync latest azure region data

--- scripts/sync-catalog.sh
+++ scripts/sync-catalog.sh
@@ -1,7 +1,7 @@
 # This script syncs latest cloud catalog data produced by azure-speed-jobs
 
 SOURCE_DIR="https://azurespeedjobs.blob.core.windows.net/jobs/cloud-catalog"
-TARGET_DIR="../ui/public/data"
+TARGET_DIR="../src/assets"
 
 curl ${SOURCE_DIR}/aws/geographies.json > ${TARGET_DIR}/aws/geographies.json
 curl ${SOURCE_DIR}/aws/regions.json > ${TARGET_DIR}/aws/regions.json
